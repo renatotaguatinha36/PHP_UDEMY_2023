8926aa8ebe4fed866a8e8aabc932bb3b737c47e3
renatoguara2020 <renatoguara2020@gmail.com>
2023-03-17 11:14:17 -0300

    ok

--- processa.php
+++ processa.php
@@ -2,5 +2,7 @@
 <?php
 
 echo "Olá " . $_POST["nome"] . " (email: " . $_POST["email"] . ")<br><br>";
+echo "O seu processador é:" . $_POST["processador"];
 echo "Sua mensagem: " . $_POST["mensagem"];
+ $livros[] =  $_POST['livros'];
 ?>
